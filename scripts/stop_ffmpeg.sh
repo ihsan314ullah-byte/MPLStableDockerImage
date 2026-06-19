0457fca8954cff91beccaa60f12c5ae7c3aa698a
ihsan314ullah-byte <ihsan314ullah@gmail.com>
2026-06-19 15:08:35 -0400

    Update stop_ffmpeg.sh

--- scripts/stop_ffmpeg.sh
+++ scripts/stop_ffmpeg.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
-# ---------------------------------------
-# Stop FFmpeg SRT caller
-# ---------------------------------------
+
+# ------------------------------------------------------------
+# STOP FFmpeg SRT Caller STREAMING PROCESS
+# ------------------------------------------------------------
 
 PID_FILE=/home/ubuntu/streaming-demo/logs/ffmpeg.pid
 
+
+# Check if PID file exists
 if [ -f "$PID_FILE" ]; then
+
     PID=$(cat "$PID_FILE")
+
     echo "Stopping FFmpeg PID $PID"
+
+    # Kill process
     kill "$PID"
-    # Wait for process to actually stop
-    while ps -p "$PID" > /dev/null; do sleep 1; done
+
+    # Wait until process fully stops
+    while ps -p "$PID" > /dev/null; do
+        sleep 1
+    done
+
+    # Remove PID file after stopping
     rm "$PID_FILE"
+
     echo "FFmpeg stopped"
+
 else
     echo "No FFmpeg PID found"
 fi
